0e6ad8423dc86b1af0cd2bdf836c6e3a70c8a281
Carlos Eduardo <ceinvestimentosllc@gmail.com>
2022-06-27 17:05:50 -0300

    Enviando questões 5,6,7 e 8 do trabalho final, faltando apenas a questão 10

---
+++ q8/script.js
@@ -0,0 +1,22 @@
+
+
+function vermelho(){
+    let y = document.querySelector('body');
+    y.style.backgroundColor = 'red';
+}
+function verde(){
+    let y = document.querySelector('body');
+    y.style.backgroundColor = 'green';
+}
+function azul(){
+    let y = document.querySelector('body');
+    y.style.backgroundColor = 'blue';
+}
+function amarelo(){
+    let y = document.querySelector('body');
+    y.style.backgroundColor = 'yellow';
+}
+
+
+
+
